8076b1cca939cd66fb3d10620a0c8f238f36cdb6
Hristijan Chochevski <Hristijan.Chochevski@datalex.com>
2023-07-19 12:42:02 +0200

    adding example about shallow copy by creating new list from existing list

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+
+        copyListExample();
 
 //        List<Integer> numbers= new LinkedList<>(List.of(1, 5, 7, 10));
         List<Integer> numbers= new CopyOnWriteArrayList<>(List.of(1,5,7,10));
@@ -21,5 +22,50 @@ public class Main {
                 Thread.sleep(2000);
  //           }
         }
+    }
+
+    private static void copyListExample() {
+
+        copyListExampleWithArrayList();
+
+        copyListExampleWithCopyOnWriteArrayList();
+    }
+
+    private static void copyListExampleWithArrayList() {
+        List<Person> people=new ArrayList<>(
+                List.of(
+                        new Person("Hristijan",24),
+                        new Person("Darko",21),
+                        new Person("Bojana",23))
+        );
+        System.out.println("Test ArrayList...");
+        List<Person> peopleCopy=new ArrayList<>(people);
+        testObjectReferences(people,peopleCopy);
+    }
+
+    private static void copyListExampleWithCopyOnWriteArrayList() {
+
+        List<Person> people=new CopyOnWriteArrayList<>(
+                List.of(
+                        new Person("Hristijan",24),
+                        new Person("Darko",21),
+                        new Person("Bojana",23))
+        );
+
+        System.out.println("Test CopyOnWriteArrayList...");
+        List<Person> peopleCopy=new CopyOnWriteArrayList<>(people);
+        testObjectReferences(people,peopleCopy);
+    }
+
+    private static void testObjectReferences(List<Person> people,List<Person> peopleCopy) {
+
+        if (people.get(0)==peopleCopy.get(0))
+            System.out.println("Same Person object");
+        else
+            System.out.println("Different Person object");
+
+        people.get(0).setName("Hristijan Ch");
+
+        System.out.println(peopleCopy.get(0).getName());
     }
 }
